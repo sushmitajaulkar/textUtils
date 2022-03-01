90a0ce3c2353287f2acc949e2e939a2a5bf48656
Sushmita A Jaulkar <86394207+sushmitajaulkar@users.noreply.github.com>
2022-03-01 19:57:59 +0530

    Dev1 (#2)
    * add random coment
    
    * code1

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,9 +1,12 @@
 // import { render } from "@testing-library/react";
 import React, {useState} from "react";
-
+// import {createUndoRedo} from 'react-undo-redo'
 
 export default function TextForm(props) {
+  const [text, setText]= useState('');
   const [count, setCount]= useState(0);
+  // const [useUndoRedo] = createUndoRedo(reducer);
+  // const [undo, redo] = useUndoRedo();
   const handleUpClick = () => {
     // console.log("Uppercase was clicked"+ text);
     let newText = text.toUpperCase();
@@ -18,42 +21,71 @@ export default function TextForm(props) {
 
   const handleClrClick = () => {
     // console.log();
-    let newText = '';
+    let newText = ' ';
     setText(newText);
   }
+
+  const handleCopy = () => {
+    let text = document.getElementById("mybox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  }
+
+  const handleExtraSpaces = () => {
+    // console.log();
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  }
+  // const handleUnClick = () => {
+  //   // console.log();
+  //   let newText = text.undo();
+  //   setText(newText);
+  // }
+
+  // const handleReClick = () => {
+  //   // console.log();
+  //   let newText = text.redo();
+  //   setText(newText);
+  // }
 
   const handleOnChange = (event) => {
     // console.log("On Change");
           setText(event.target.value);
           setCount(event.target.value.split('.').length);
-  }
-
-  const [text, setText]= useState('');
-  
-  // render();{
-  //     console.log(this.state.length);
-  
+        }
+   
   return (
     <>
-      <div className="container">
+      <div className="container" style={{color: props.mode=== 'dark'?'white':'#002b80'}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-          <textarea className="form-control" value= {text} onChange={handleOnChange} id="mybox" rows="8"></textarea>
+          <textarea className="form-control" value= {text} onChange={handleOnChange} style={{backgroundColor: props.mode=== 'dark'?'#002b80':'white', 
+          color:props.mode=== 'dark'?'white':'#002b80'}} id="mybox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
-        <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to Lowercase</button>
-        <button className="btn btn-primary mx-2" onClick={handleClrClick}>Clear</button>
+        
+        <button className="btn btn-primary mx-2 btn-info" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-2 btn-info" onClick={handleLowClick}>Convert to Lowercase</button>
+        <button className="btn btn-primary mx-2 btn-info" onClick={handleClrClick}>Clear</button>
+        <button className="btn btn-primary mx-2 btn-info" onClick={handleCopy}>Copy</button>
+        <button className="btn btn-primary mx-2 btn-info" onClick={handleExtraSpaces}>Remove Extra Space</button>
 
+       {/* <button disabled={!undo.isPossible} onClick={() => undo()}>
+        Undo
+      </button>
+
+      <button disabled={!redo.isPossible} onClick={() => redo()}>
+        Redo
+      </button> */}
       </div>
 
-      <div className="container my-3">
+      <div className="container my-3" style={{color: props.mode=== 'dark'?'white':'#002b80'}}>
          <h2>Your text summary</h2>
          <p>{text.split(" ").length} words</p>
          <p>{text.length} characters </p>
          <p>{count}sentences</p>   
          <p>{0.008 * text.split(" ").length} Minutes read</p>
          <h2> Preview </h2>
-         <p>{text}</p>
+         <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
       </div>
 
     </>
